81807af737a442f784c113b7ed563483f23a2853
Cylindric <mark@hanfordonline.co.uk>
2025-08-22 00:34:21 +0100

    Tutorial 13 complete

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -2,10 +2,12 @@ extends NodeState
 
 @export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
+var on_ui: bool = false
 
 
 func _on_process(_delta : float) -> void:
-	pass
+	if not Input.is_action_pressed("hit"):
+		on_ui = true
 
 
 func _on_physics_process(_delta : float) -> void:
@@ -21,18 +23,24 @@ func _on_physics_process(_delta : float) -> void:
 		animated_sprite_2d.play("idle_front")
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("hit"):
+		on_ui = false
+
+
 func _on_next_transitions() -> void:
 	GameInputEvents.movement_input()
 
 	if GameInputEvents.is_movement_input():
 		transition.emit("Walk")
 
-	if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvents.use_tool():
-		transition.emit("Chopping")
-	if player.current_tool == DataTypes.Tools.TillGround && GameInputEvents.use_tool():
-		transition.emit("Tilling")
-	if player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvents.use_tool():
-		transition.emit("Watering")
+	if !on_ui:
+		if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvents.use_tool():
+			transition.emit("Chopping")
+		if player.current_tool == DataTypes.Tools.TillGround && GameInputEvents.use_tool():
+			transition.emit("Tilling")
+		if player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvents.use_tool():
+			transition.emit("Watering")
 
 
 func _on_enter() -> void:
